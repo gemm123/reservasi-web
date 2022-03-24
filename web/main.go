89b65d4e53527e42a6ca488261092ff9000b3c6a
gemm123 <gemaakbar213@gmail.com>
2022-03-24 20:23:59 +0700

    konfigurasi server

--- web/main.go
+++ web/main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"encoding/gob"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
 	"github.com/alexedwards/scs/v2"
 	"github.com/gemm123/reservasi-web/internal/config"
 	"github.com/gemm123/reservasi-web/internal/driver"
+	"github.com/gemm123/reservasi-web/internal/handlers"
+	"github.com/gemm123/reservasi-web/internal/helpers"
 	"github.com/gemm123/reservasi-web/internal/models"
+	"github.com/gemm123/reservasi-web/internal/render"
 )
 
 var app config.AppConfig
 var session *scs.SessionManager
-
-func main() {
-
-}
 
 func run() (*driver.DB, error) {
 	//model data yang ditaruh di session
@@ -37,4 +38,49 @@ func run() (*driver.DB, error) {
 	session = scs.New()
 	session.Lifetime = 24 * time.Hour
 	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	//koneksi ke database
+	connectionString := "host=localhost port=5432 dbname=reservasi_web user=postgres password=gemmq123456"
+	db, err := driver.ConnectSQL(connectionString)
+	if err != nil {
+		log.Fatal("cant connect to database")
+	}
+	log.Println("connected to database")
+
+	templateCache, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("cannot create template cache")
+		return nil, err
+	}
+
+	app.TemplateCache = templateCache
+
+	repo := handlers.NewRepo(&app, db)
+	handlers.NewHandlers(repo)
+	render.NewRenderer(&app)
+	helpers.NewHelpers(&app)
+
+	return db, nil
+}
+
+func main() {
+	db, err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.SQL.Close()
+
+	fmt.Println("starting application on port 8080")
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: routes(&app),
+	}
+
+	err = server.ListenAndServe()
+	log.Fatal(err)
 }
