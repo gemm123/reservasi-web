10328e5d09ef26f7c5838c8a184f89cbd7dc6be1
gemm123 <gemaakbar213@gmail.com>
2022-04-09 16:18:59 +0700

    update cek form

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,9 +1,64 @@
 package forms
 
-import "net/url"
+import (
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/asaskevich/govalidator"
+)
 
 //membuat custom form
 type Form struct {
 	url.Values
 	Errors errors
+}
+
+//inisialisasi form struct
+func New(data url.Values) *Form {
+	return &Form{
+		data,
+		errors(map[string][]string{}),
+	}
+}
+
+//return true jika tidak ada error
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
+
+//cek form kosong
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "Tidak boleh kosong")
+		}
+	}
+}
+
+//cek isi form
+func (f *Form) Has(field string) bool {
+	x := f.Get(field)
+	if x == "" {
+		return false
+	}
+	return true
+}
+
+//cek minimal panjang
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("Minimal berisi %d karakter", length))
+		return false
+	}
+	return true
+}
+
+//cek valid email address
+func (f *Form) IsEmail(field string) {
+	if !govalidator.IsEmail(f.Get(field)) {
+		f.Errors.Add(field, "invalid email address")
+	}
 }
