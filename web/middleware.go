3e4668afda49b6b256988f0088ec3913095eadb3
gemm123 <gemaakbar213@gmail.com>
2022-04-13 10:09:34 +0700

    add func cek login

--- web/middleware.go
+++ web/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 
+	"github.com/gemm123/reservasi-web/internal/helpers"
 	"github.com/justinas/nosurf"
 )
 
@@ -23,4 +24,15 @@ func NoSurf(next http.Handler) http.Handler {
 //load dan save session di setiap request
 func SessionLoad(next http.Handler) http.Handler {
 	return session.LoadAndSave(next)
+}
+
+func Auth(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if !helpers.IsAuthenticated(request) {
+			session.Put(request.Context(), "error", "Login terlebih dahulu!")
+			http.Redirect(writer, request, "/login", http.StatusSeeOther)
+			return
+		}
+		next.ServeHTTP(writer, request)
+	})
 }
