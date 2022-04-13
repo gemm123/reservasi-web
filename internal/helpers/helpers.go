df968e322af81796585b32ade8cb5fc057898d2b
gemm123 <gemaakbar213@gmail.com>
2022-04-13 10:06:22 +0700

    add autentikasi

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,9 +1,19 @@
 package helpers
 
-import "github.com/gemm123/reservasi-web/internal/config"
+import (
+	"net/http"
+
+	"github.com/gemm123/reservasi-web/internal/config"
+)
 
 var app *config.AppConfig
 
+//setup app config untuk helpers
 func NewHelpers(a *config.AppConfig) {
 	app = a
+}
+
+func IsAuthenticated(request *http.Request) bool {
+	exist := app.Session.Exists(request.Context(), "user_id")
+	return exist
 }
