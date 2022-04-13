d86a07ef35083ca72a5f28a45dc62f7541241e90
gemm123 <gemaakbar213@gmail.com>
2022-04-13 10:06:02 +0700

    update handlers

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gemm123/reservasi-web/internal/config"
 	"github.com/gemm123/reservasi-web/internal/driver"
+	"github.com/gemm123/reservasi-web/internal/forms"
 	"github.com/gemm123/reservasi-web/internal/models"
 	"github.com/gemm123/reservasi-web/internal/render"
 	"github.com/gemm123/reservasi-web/internal/repository"
@@ -40,21 +42,113 @@ func (repo *Repository) About(writer http.ResponseWriter, request *http.Request)
 	render.Template(writer, request, "about.page.html", &models.TemplateData{})
 }
 
-func (repo *Repository) Kopi(writer http.ResponseWriter, request *http.Request) {
-	render.Template(writer, request, "kopi.page.html", &models.TemplateData{})
+func (repo *Repository) President(writer http.ResponseWriter, request *http.Request) {
+	render.Template(writer, request, "president.page.html", &models.TemplateData{})
 }
 
-func (repo *Repository) Teh(writer http.ResponseWriter, request *http.Request) {
-	render.Template(writer, request, "teh.page.html", &models.TemplateData{})
+func (repo *Repository) Royal(writer http.ResponseWriter, request *http.Request) {
+	render.Template(writer, request, "royal.page.html", &models.TemplateData{})
 }
 
-func (repo *Repository) Susu(writer http.ResponseWriter, request *http.Request) {
-	render.Template(writer, request, "susu.page.html", &models.TemplateData{})
+func (repo *Repository) TowerClub(writer http.ResponseWriter, request *http.Request) {
+	render.Template(writer, request, "tower-club.page.html", &models.TemplateData{})
 }
 
-func (repo *Repository) Jahe(writer http.ResponseWriter, request *http.Request) {
-	render.Template(writer, request, "jahe.page.html", &models.TemplateData{})
+func (repo *Repository) GrandDeluxe(writer http.ResponseWriter, request *http.Request) {
+	render.Template(writer, request, "grand-deluxe.page.html", &models.TemplateData{})
 }
-func (repo *Repository) Jus(writer http.ResponseWriter, request *http.Request) {
-	render.Template(writer, request, "jus.page.html", &models.TemplateData{})
+
+func (repo *Repository) Deluxe(writer http.ResponseWriter, request *http.Request) {
+	render.Template(writer, request, "deluxe.page.html", &models.TemplateData{})
+}
+
+func (repo *Repository) ShowLogin(writer http.ResponseWriter, request *http.Request) {
+	render.Template(writer, request, "login.page.html", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (repo *Repository) Register(writer http.ResponseWriter, request *http.Request) {
+	render.Template(writer, request, "register.page.html", &models.TemplateData{
+		Form: forms.New(nil),
+	})
+}
+
+func (repo *Repository) PostRegister(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		repo.App.Session.Put(request.Context(), "error", "Gagal parse form!")
+		http.Redirect(writer, request, "/register", http.StatusSeeOther)
+		return
+	}
+
+	name := request.Form.Get("name")
+	email := request.Form.Get("email")
+	password := request.Form.Get("password")
+
+	form := forms.New(request.PostForm)
+	form.Required("name", "email", "password")
+	form.MinLength("name", 3)
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		render.Template(writer, request, "register.page.html", &models.TemplateData{
+			Form: form,
+		})
+		return
+	}
+
+	err = repo.DB.InsertUser(name, email, password)
+	if err != nil {
+		repo.App.Session.Put(request.Context(), "error", "Gagal register user!")
+		http.Redirect(writer, request, "/register", http.StatusSeeOther)
+		return
+	}
+
+	repo.App.Session.Put(request.Context(), "success", "Register berhasil")
+	http.Redirect(writer, request, "/login", http.StatusSeeOther)
+}
+
+func (repo *Repository) PostShowLogin(writer http.ResponseWriter, request *http.Request) {
+	_ = repo.App.Session.RenewToken(request.Context())
+
+	err := request.ParseForm()
+	if err != nil {
+		log.Println(err)
+	}
+
+	email := request.Form.Get("email")
+	password := request.Form.Get("password")
+
+	form := forms.New(request.PostForm)
+	form.Required("email", "password")
+	form.IsEmail("email")
+
+	if !form.Valid() {
+		render.Template(writer, request, "login.page.html", &models.TemplateData{
+			Form: form,
+		})
+		return
+	}
+
+	id, _, err := repo.DB.Authenticated(email, password)
+	if err != nil {
+		repo.App.Session.Put(request.Context(), "error", "Email atau password salah")
+		http.Redirect(writer, request, "/login", http.StatusSeeOther)
+		return
+	}
+
+	repo.App.Session.Put(request.Context(), "user_id", id)
+	repo.App.Session.Put(request.Context(), "success", "Berhasil login")
+	http.Redirect(writer, request, "/", http.StatusSeeOther)
+}
+
+func (repo *Repository) Logout(writer http.ResponseWriter, request *http.Request) {
+	_ = repo.App.Session.Destroy(request.Context())
+	_ = repo.App.Session.RenewToken(request.Context())
+	http.Redirect(writer, request, "/login", http.StatusSeeOther)
+}
+
+func (repo *Repository) AdminDashboard(writer http.ResponseWriter, request *http.Request) {
+	render.Template(writer, request, "admin-dashboard.page.html", &models.TemplateData{})
 }
