e21628ad451fe44b4c0acb4dc27ffd628c6f1cf4
gemm123 <gemaakbar213@gmail.com>
2022-04-26 21:18:55 +0700

    add seed for rooms and restrictions

---
+++ migrations/20220426131431_create_seed_restrictions_tables.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name, created_at, updated_at) VALUES
+    ('Reservasi', '2022-04-26 00:00:00.000', '2022-04-26 00:00:00.000'),
+    ('Perbaikan', '2022-04-26 00:00:00.000', '2022-04-26 00:00:00.000');
