f0fbb05f14ece8a05d2434238a6c6196b98f9f8a
gemm123 <gemaakbar213@gmail.com>
2022-04-27 20:26:46 +0700

    add search availability database

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,6 +1,9 @@
 package repository
 
+import "time"
+
 type DatabaseRepo interface {
 	InsertUser(name, email, password string) error
 	Authenticated(email, password string) (int, string, error)
+	SearchAvailabilityByDatesAndRoomID(start, end time.Time, roomID int) (bool, error)
 }
