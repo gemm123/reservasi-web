f44daaae07f7deb1e6adba1c4d19e5f1abf6813b
gemm123 <gemaakbar213@gmail.com>
2022-04-27 20:27:21 +0700

    add search-availability route

--- web/routes.go
+++ web/routes.go
@@ -22,12 +22,6 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
-	mux.Get("/login", handlers.Repo.ShowLogin)
-	mux.Post("/login", handlers.Repo.PostShowLogin)
-	mux.Get("/register", handlers.Repo.Register)
-	mux.Post("/register", handlers.Repo.PostRegister)
-	mux.Get("/logout", handlers.Repo.Logout)
-
 	mux.Route("/room", func(mux chi.Router) {
 		mux.Get("/president", handlers.Repo.President)
 		mux.Get("/royal", handlers.Repo.Royal)
@@ -35,6 +29,14 @@ func routes(app *config.AppConfig) http.Handler {
 		mux.Get("/grand-deluxe", handlers.Repo.GrandDeluxe)
 		mux.Get("/deluxe", handlers.Repo.Deluxe)
 	})
+
+	mux.Post("/search-availability", handlers.Repo.CheckAvailability)
+
+	mux.Get("/login", handlers.Repo.ShowLogin)
+	mux.Post("/login", handlers.Repo.PostShowLogin)
+	mux.Get("/register", handlers.Repo.Register)
+	mux.Post("/register", handlers.Repo.PostRegister)
+	mux.Get("/logout", handlers.Repo.Logout)
 
 	mux.Route("/admin", func(mux chi.Router) {
 		mux.Use(Auth)
